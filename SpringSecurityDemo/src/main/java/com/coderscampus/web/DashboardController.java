3e5bec806c08b9c6c06b2538b0b949c6d20e0910
shatakshi <rastogi.prakhar@live.com>
2020-06-16 16:18:28 +0530

    first commit

---
+++ SpringSecurityDemo/src/main/java/com/coderscampus/web/DashboardController.java
@@ -0,0 +1,12 @@
+package com.coderscampus.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+  @GetMapping("/dashboard")
+  public String getDashboard () {
+    return "dashboard";
+  }
+}
